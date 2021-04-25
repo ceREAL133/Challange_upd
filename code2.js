e81865623c27871fdac05c0b92f034764c9e2f8a
ceREAL133 <72147047+ceREAL133@users.noreply.github.com>
2021-04-25 10:07:08 +0300

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,26 @@
+const userInput = document.getElementById('userInput');
+let textholder = document.querySelector('.textHolder')
+let body = document.querySelector('.body');
+
+const regex = new RegExp ('^[a-zA-z\\s]*$');
+const tmp = userInput.value;
+let save = '';
+userInput.oninput = () => {
+    !regex.test(userInput.value) ? userInput.value = save : save = userInput.value
+}
+
+const colorsMapping = {
+    red: ['stop', 'red'],
+    yellow: ['wait', 'yellow'],
+    green: ['go', 'green'],
+    
+    default: ['enter valid', 'white'],
+}
+
+function checkColor() {
+    const value = userInput.value.toLowerCase();
+    const [textContent, backgroundColor] = colorsMapping[value] || colorsMapping.default;
+    
+    body.style.backgroundColor = backgroundColor;
+    textholder.textContent = textContent;
+}
